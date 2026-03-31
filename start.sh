de4f6f1a9e9535422ca54a5f1b7d322a27545401
MO2026 <mohamedahmed66972007@gmail.com>
2026-03-31 07:25:41 +0300

    UBDATE

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start the API server in the background
-export PORT=3000
-export NODE_ENV=development
-(cd /home/runner/workspace/artifacts/api-server && pnpm run dev) &
-API_PID=$!
-
